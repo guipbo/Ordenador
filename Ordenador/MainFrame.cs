85cbee2855455e131cb6a9470f93feacaeda949b
Guilherme Braga <guipbo@gmail.com>
2020-11-19 09:58:15 -0300

    Números decimais e negativos

--- Ordenador/MainFrame.cs
+++ Ordenador/MainFrame.cs
@@ -13,7 +13,7 @@ namespace Ordenador
     public partial class fOrdenador : Form
     {
 
-        private List<int> array = new List<int>();
+        private List<double> array = new List<double>();
 
         public fOrdenador()
         {
@@ -22,34 +22,57 @@ namespace Ordenador
 
         private void tbDigite_KeyPress(object sender, KeyPressEventArgs e)
         {
-            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            if ((!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != ',') && (e.KeyChar != '-')) 
+                || (e.KeyChar == '-' && (sender as TextBox).Text.Length > 0)
+                || ((e.KeyChar == ',') && ((sender as TextBox).Text.IndexOf(',') > -1)))
             {
                 e.Handled = true;
-                errorProvider1.SetError(lErro, "Digite um número inteiro.");
-                lErro.Text = "Digite um número inteiro.";
+                errorProvider1.SetError(lErro, "Digite um número.");
+                lErro.Text = "Digite um número.";
                 return;
             }
-            
+
             errorProvider1.SetError(lErro, "");
             lErro.Text = "";
         }
 
         private void bConfirmar_Click(object sender, EventArgs e)
         {
-            if(tbDigite.Text.Length == 0)
+            if(tbDigite.Text.Length == 0 
+                || tbDigite.Text.EndsWith("-") 
+                || tbDigite.Text.EndsWith(","))
             {
-                lErro.Text = "Digite um número inteiro.";
+                errorProvider1.SetError(lErro, "Digite um número.");
+                lErro.Text = "Digite um número.";
                 tbDigite.Clear();
                 tbDigite.Focus();
                 return;
             }
+
+            atualizarLista();
+            somar();
             
-            int temp = 0;
+            tbDigite.Clear();
+            tbDigite.Focus();
+        }
+
+        private void bLimpar_Click(object sender, EventArgs e)
+        {
+            array = new List<double>();
+            tbSoma.Text = "0";
+            tbLista.Clear();
+            tbDigite.Clear();
+            tbDigite.Focus();
+        }
+
+        private void atualizarLista()
+        {
+            array.Add(Convert.ToDouble(tbDigite.Text));
+            double temp = 0;
             tbLista.Text = "";
-            array.Add(Convert.ToInt32(tbDigite.Text));
-            for (int j=0; j < array.Count; j++)
+            for (int j = 0; j < array.Count; j++)
             {
-                for(int i= j+1; i < array.Count; i++)
+                for (int i = j + 1; i < array.Count; i++)
                 {
                     if (array[j] > array[i])
                     {
@@ -58,25 +81,15 @@ namespace Ordenador
                         array[i] = temp;
                     }
                 }
-                tbLista.Text = tbLista.Text + array[j].ToString() + ", ";
+                tbLista.Text = tbLista.Text + array[j].ToString() + "; ";
             }
             tbLista.Text = tbLista.Text + "(...)";
-
-
-            int soma = Convert.ToInt32(tbSoma.Text) + Convert.ToInt32(tbDigite.Text);
-            tbSoma.Text = soma.ToString();
-
-            tbDigite.Clear();
-            tbDigite.Focus();
         }
 
-        private void bLimpar_Click(object sender, EventArgs e)
+        private void somar()
         {
-            array = new List<int>();
-            tbSoma.Text = "0";
-            tbLista.Clear();
-            tbDigite.Clear();
-            tbDigite.Focus();
+            double soma = Convert.ToDouble(tbSoma.Text) + Convert.ToDouble(tbDigite.Text);
+            tbSoma.Text = soma.ToString();
         }
     }
 }
