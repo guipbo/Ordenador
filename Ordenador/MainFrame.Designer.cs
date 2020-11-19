e50db5d42fbe39d3f1b9be85a32607165c59888a
Guilherme Braga <guipbo@gmail.com>
2020-11-18 22:37:10 -0300

    Project

---
+++ Ordenador/MainFrame.Designer.cs
@@ -0,0 +1,203 @@
+﻿
+namespace Ordenador
+{
+    partial class fOrdenador
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bConfirmar = new System.Windows.Forms.Button();
+            this.lDigite = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.lLista = new System.Windows.Forms.Label();
+            this.lSoma = new System.Windows.Forms.Label();
+            this.tbSoma = new System.Windows.Forms.TextBox();
+            this.bLimpar = new System.Windows.Forms.Button();
+            this.tbDigite = new System.Windows.Forms.TextBox();
+            this.lErro = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.tbLista = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bConfirmar
+            // 
+            this.bConfirmar.BackColor = System.Drawing.Color.LightGreen;
+            this.bConfirmar.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.bConfirmar.FlatAppearance.BorderSize = 0;
+            this.bConfirmar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.bConfirmar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.bConfirmar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bConfirmar.Location = new System.Drawing.Point(283, 73);
+            this.bConfirmar.Margin = new System.Windows.Forms.Padding(0);
+            this.bConfirmar.Name = "bConfirmar";
+            this.bConfirmar.Size = new System.Drawing.Size(97, 26);
+            this.bConfirmar.TabIndex = 1;
+            this.bConfirmar.Text = "Confirmar";
+            this.toolTip1.SetToolTip(this.bConfirmar, "Adicionar um número na lista");
+            this.bConfirmar.UseVisualStyleBackColor = false;
+            this.bConfirmar.Click += new System.EventHandler(this.bConfirmar_Click);
+            // 
+            // lDigite
+            // 
+            this.lDigite.AutoSize = true;
+            this.lDigite.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lDigite.Location = new System.Drawing.Point(28, 76);
+            this.lDigite.Name = "lDigite";
+            this.lDigite.Size = new System.Drawing.Size(134, 20);
+            this.lDigite.TabIndex = 1;
+            this.lDigite.Text = "Digite um número";
+            // 
+            // lLista
+            // 
+            this.lLista.AutoSize = true;
+            this.lLista.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lLista.Location = new System.Drawing.Point(28, 148);
+            this.lLista.Name = "lLista";
+            this.lLista.Size = new System.Drawing.Size(144, 20);
+            this.lLista.TabIndex = 5;
+            this.lLista.Text = "Números Digitados";
+            this.lLista.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lSoma
+            // 
+            this.lSoma.AutoSize = true;
+            this.lSoma.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lSoma.Location = new System.Drawing.Point(28, 236);
+            this.lSoma.Name = "lSoma";
+            this.lSoma.Size = new System.Drawing.Size(51, 20);
+            this.lSoma.TabIndex = 6;
+            this.lSoma.Text = "Soma";
+            this.lSoma.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // tbSoma
+            // 
+            this.tbSoma.BackColor = System.Drawing.SystemColors.Info;
+            this.tbSoma.Enabled = false;
+            this.tbSoma.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbSoma.Location = new System.Drawing.Point(86, 233);
+            this.tbSoma.Name = "tbSoma";
+            this.tbSoma.Size = new System.Drawing.Size(133, 26);
+            this.tbSoma.TabIndex = 7;
+            this.tbSoma.TabStop = false;
+            this.tbSoma.Text = "0";
+            this.toolTip1.SetToolTip(this.tbSoma, "Soma dos números digitados");
+            // 
+            // bLimpar
+            // 
+            this.bLimpar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.bLimpar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.bLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bLimpar.Location = new System.Drawing.Point(302, 230);
+            this.bLimpar.Name = "bLimpar";
+            this.bLimpar.Size = new System.Drawing.Size(78, 29);
+            this.bLimpar.TabIndex = 2;
+            this.bLimpar.TabStop = false;
+            this.bLimpar.Text = "Limpar";
+            this.toolTip1.SetToolTip(this.bLimpar, "Limpar campos");
+            this.bLimpar.UseVisualStyleBackColor = false;
+            this.bLimpar.Click += new System.EventHandler(this.bLimpar_Click);
+            // 
+            // tbDigite
+            // 
+            this.tbDigite.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbDigite.Location = new System.Drawing.Point(166, 73);
+            this.tbDigite.Name = "tbDigite";
+            this.tbDigite.Size = new System.Drawing.Size(114, 26);
+            this.tbDigite.TabIndex = 0;
+            this.tbDigite.TextChanged += new System.EventHandler(this.tbDigite_TextChanged);
+            this.tbDigite.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbDigite_KeyPress);
+            // 
+            // lErro
+            // 
+            this.lErro.AutoSize = true;
+            this.lErro.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lErro.Location = new System.Drawing.Point(163, 111);
+            this.lErro.Name = "lErro";
+            this.lErro.Size = new System.Drawing.Size(0, 20);
+            this.lErro.TabIndex = 8;
+            this.lErro.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // tbLista
+            // 
+            this.tbLista.BackColor = System.Drawing.SystemColors.Info;
+            this.tbLista.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.tbLista.Enabled = false;
+            this.tbLista.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbLista.Location = new System.Drawing.Point(32, 172);
+            this.tbLista.Multiline = true;
+            this.tbLista.Name = "tbLista";
+            this.tbLista.ReadOnly = true;
+            this.tbLista.Size = new System.Drawing.Size(348, 52);
+            this.tbLista.TabIndex = 9;
+            this.tbLista.TabStop = false;
+            this.toolTip1.SetToolTip(this.tbLista, "Números digitados, em ordem crescente");
+            // 
+            // fOrdenador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(406, 313);
+            this.Controls.Add(this.tbLista);
+            this.Controls.Add(this.lErro);
+            this.Controls.Add(this.tbDigite);
+            this.Controls.Add(this.bLimpar);
+            this.Controls.Add(this.tbSoma);
+            this.Controls.Add(this.lSoma);
+            this.Controls.Add(this.lLista);
+            this.Controls.Add(this.lDigite);
+            this.Controls.Add(this.bConfirmar);
+            this.Name = "fOrdenador";
+            this.Text = "Ordenador";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bConfirmar;
+        private System.Windows.Forms.Label lDigite;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label lLista;
+        private System.Windows.Forms.Label lSoma;
+        private System.Windows.Forms.TextBox tbSoma;
+        private System.Windows.Forms.Button bLimpar;
+        private System.Windows.Forms.TextBox tbDigite;
+        private System.Windows.Forms.Label lErro;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.TextBox tbLista;
+    }
+}
+
---
+++ Ordenador/MainFrame.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ordenador
+{
+    public partial class fOrdenador : Form
+    {
+
+        private List<int> array = new List<int>();
+
+        public fOrdenador()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fontDialog1_Apply(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbDigite_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbDigite_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+                errorProvider1.SetError(lErro, "Digite um número inteiro.");
+                lErro.Text = "Digite um número inteiro.";
+                return;
+            }
+            
+            errorProvider1.SetError(lErro, "");
+            lErro.Text = "";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bConfirmar_Click(object sender, EventArgs e)
+        {
+            if(tbDigite.Text.Length == 0)
+            {
+                lErro.Text = "Digite um número inteiro.";
+                tbDigite.Clear();
+                tbDigite.Focus();
+                return;
+            }
+            
+            int temp = 0;
+            tbLista.Text = "";
+            array.Add(Convert.ToInt32(tbDigite.Text));
+            for (int j=0; j < array.Count; j++)
+            {
+                for(int i= j+1; i < array.Count; i++)
+                {
+                    if (array[j] > array[i])
+                    {
+                        temp = array[j];
+                        array[j] = array[i];
+                        array[i] = temp;
+                    }
+                }
+                tbLista.Text = tbLista.Text + array[j].ToString() + ", ";
+            }
+            tbLista.Text = tbLista.Text + "(...)";
+
+
+            int soma = Convert.ToInt32(tbSoma.Text) + Convert.ToInt32(tbDigite.Text);
+            tbSoma.Text = soma.ToString();
+
+            tbDigite.Clear();
+            tbDigite.Focus();
+        }
+
+        private void tbDigite_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void bLimpar_Click(object sender, EventArgs e)
+        {
+            array = new List<int>();
+            tbSoma.Text = "0";
+            tbLista.Clear();
+            tbDigite.Clear();
+            tbDigite.Focus();
+        }
+    }
+}
